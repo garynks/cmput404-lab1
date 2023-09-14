589e6cfcf4557830020ea61ddf64b45af283cb09
Gary Ng Kwong Sang <gng4820@gmail.com>
2023-09-13 18:57:08 -0600

    Modified Py Script to include raw URL

--- lab1.py
+++ lab1.py
@@ -1,8 +1,8 @@
 import requests
+print("requests version:", requests.__version__)
 
-print(requests.__version__)
+# resp = requests.get("http://www.google.com")
+# print(resp.text)
 
-resp = requests.get("http://www.google.com")
-
-print(resp.text)
-
+script = requests.get("https://raw.githubusercontent.com/garynks/cmput404-lab1/master/lab1.py")
+print(script.text)
